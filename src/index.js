ad4a5f01830e69cfd0e3fd05acd94444c165ecf5
Matt Beaudoin <ehxtwo@gmail.com>
2021-06-07 16:49:27 -0700

    commit tree 0ec6d45eeb101e2dfb7a6ac6ee521663ae29edea

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
 console.log('%c HI', 'color: firebrick')
+
+
+fetch ("https://dog.ceo/api/breeds/image/random/4")
+    .then(response => response.json())
+    .then(data => console.log(data));
+
+
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
